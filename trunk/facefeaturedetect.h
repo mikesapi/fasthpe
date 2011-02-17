b317166847c139f26a0e4ac6c2ee32f1bc72ced3
mikesapi <mikesapi@4296d0d1-6f10-4ad9-9b36-b4bc55292e57>
2011-02-17 14:46:44 +0000

    modified headers to avoid possibility of multiple inclusion, included GPL Licence
    git-svn-id: https://subversion.assembla.com/svn/openhpe@11 4296d0d1-6f10-4ad9-9b36-b4bc55292e57

--- trunk/facefeaturedetect.h
+++ trunk/facefeaturedetect.h
@@ -1,9 +1,28 @@
-// Header File Feature_detect.h
+/*
+Copyright (C) 2010 Michael Sapienza
+   
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FACE_FEATURE_DETECT_H
+#define FACE_FEATURE_DETECT_H
+
 int initFaceDet(const char * faceCascadePath,
-				const char * noseCascadePath,
-				const char * eyesCascadePath,
-				const char * mouthCascadePath);
-
-void     closeFaceDet();
-
+		const char * noseCascadePath,
+		const char * eyesCascadePath,
+		const char * mouthCascadePath);
+void closeFaceDet();
 IplImage* detect_features( IplImage* img );
+
+#endif
--- trunk/pose-estimation.h
+++ trunk/pose-estimation.h
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2010 Michael Sapienza
+   
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef POSE_ESTIMATION_H
+#define POSE_ESTIMATION_H
+
 IplImage* draw_and_calculate( IplImage* img );
 float	FindDistance	(CvPoint pt1, CvPoint pt2);
 float	FindAngle		(CvPoint pt1, CvPoint pt2);
@@ -10,3 +30,5 @@ void    init_kalman_filter(void);
 void 	print_text(IplImage* img, int counter, CvScalar colour);
 void 	draw_trail(IplImage* img, CvPoint* pt);
 void 	closeDraw();
+
+#endif
